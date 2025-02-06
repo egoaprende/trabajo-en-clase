55d35cd966e039e58fba35911b9a13dc373c9179
egoaprende <unturnet2030@gmail.com>
2025-02-05 19:52:36 -0600

    starting

--- Polimorfismo2.cpp
+++ Polimorfismo2.cpp
@@ -13,21 +13,30 @@ class Circulo : public Figura {
     }
 };
 
-class Cuadrado : public figura{
+class Cuadrado : public Figura{
     void dibujar() override {
         cout<<"Dibujando cuadrado"<< endl;
+    }
+};
+
+class Triangulo : public Figura{
+    void dibujar() override {
+        cout<<"Dibujando Triangulo"<< endl;
     }
 };
 
 int main(){
     Figura* f1 = new Circulo();
     Figura* f2 = new Cuadrado();
+    Figura* f3 = new Triangulo();
 
     f1-> dibujar();
     f2-> dibujar();
+    f3-> dibujar();
 
     delete f1;
     delete f2;
+    delete f3;
 
     return 0;
 }
